4621ad823e91ba5dd58b63009090dee40da24c30
PaulRwagasana <p.rwagasana@alustudent.com>
2024-12-03 16:49:08 +0200

    Adv

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+num = 122
+while num > 96:
+    print("{}".format(chr(num) if num % 2 == 0 else chr(num - 32)), end="")
+    num -= 1
