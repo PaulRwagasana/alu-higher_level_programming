247b872b035f44fa16c3aac919d064ca80a6711b
PaulRwagasana <p.rwagasana@alustudent.com>
2024-12-03 18:31:27 +0200

    Update 101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
+"""
+    define a function 'print_stats'
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
     """
+        print accumulated metrics
+    """
+
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
